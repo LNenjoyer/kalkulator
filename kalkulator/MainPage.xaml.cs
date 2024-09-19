da004cd46b591ea46316857bf30f128742c8f214
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-09-19 09:37:22 +0200

    zmainy

---
+++ kalkulator/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace kalkulator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnWybranaLiczba(object sender, EventArgs e) 
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+        }
+        void OnWybranyOperator(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+        }
+        void OnWynik(object sender, EventArgs e) { }
+
+
+
+    }
+    
+
+}
