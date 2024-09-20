59537d0ff1b1cbc3b15382e3da432c5f8f65c865
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-09-20 19:27:53 +0200

    zmiany

--- kalkulator/MainPage.xaml.cs
+++ kalkulator/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace kalkulator
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kalkulator
 {
     public partial class MainPage : ContentPage
     {
@@ -6,21 +8,93 @@
         public MainPage()
         {
             InitializeComponent();
+            OnClear(this, null);
         }
-        void OnWybranaLiczba(object sender, EventArgs e) 
+        string currentEntry = "";
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+        string decimalFormat = "N0";
+
+        void OnWybranaLiczba(object sender, EventArgs e)
         {
+
             Button button = (Button)sender;
             string pressed = button.Text;
+
+            currentEntry += pressed;
+
+            if ((this.wynik.Text == "0" && pressed == "0")
+                || (currentEntry.Length <= 1 && pressed != "0")
+                || currentState < 0)
+            {
+                this.wynik.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            if (pressed == "." && decimalFormat != "N2")
+            {
+                decimalFormat = "N2";
+            }
+
+            this.wynik.Text += pressed;
         }
         void OnWybranyOperator(object sender, EventArgs e)
         {
+            LockNumberValue(wynik.Text);
+
+            currentState = -2;
             Button button = (Button)sender;
             string pressed = button.Text;
+            mathOperator = pressed;
         }
-        void OnWynik(object sender, EventArgs e) { }
+        private void LockNumberValue(string text)
+        {
+            double number;
+            if (double.TryParse(text, out number))
+            {
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
 
+                currentEntry = string.Empty;
+            }
+        }
 
+        void OnWynik(object sender, EventArgs e)
+        {
+            if (currentState == 2)
+            {
+                if (secondNumber == 0)
+                    LockNumberValue(wynik.Text);
 
+                double result = Calculator.Calculator.Calculate(firstNumber, secondNumber, mathOperator);
+
+                this.obecnie.Text = $"{firstNumber} {mathOperator} {secondNumber}";
+
+                this.wynik.Text = Calculator.DoubleExtensions.ToTrimmedString(result,decimalFormat);
+                firstNumber = result;
+                secondNumber = 0;
+                currentState = -1;
+                currentEntry = string.Empty;
+            }
+        }
+
+        void OnClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            decimalFormat = "N0";
+            this.wynik.Text = "0";
+            currentEntry = string.Empty;
+        }
     }
     
 
--- kalkulator/obj/Debug/net8.0-android/kalkulator.AssemblyInfo.cs
+++ kalkulator/obj/Debug/net8.0-android/kalkulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kalkulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da004cd46b591ea46316857bf30f128742c8f214")]
 [assembly: System.Reflection.AssemblyProductAttribute("kalkulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kalkulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
